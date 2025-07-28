531e88754811e0532e09b41b6771755a84be95db
khandakershahi <khandakershahi@gmail.com>
2025-07-28 19:26:08 +0600

    initial commit

---
+++ 1_Free_Drinks.js
@@ -0,0 +1,16 @@
+/***
+*
+* Free Drinks
+*   - Burger more than 500tk: free Coke
+*   - Else Coke: 30tk
+*/
+
+var burgerPrice = 501;
+var cokePrice = 30;
+
+
+if(burgerPrice > 500){
+    console.log('You get a free coke');
+}else{
+    console.log(burgerPrice + cokePrice);
+}
